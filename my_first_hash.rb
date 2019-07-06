69009f27f78431e4d0302cbf7699469e6fcfbf68
Sarah Harrs <sarahharrs@gmail.com>
2019-07-06 17:49:18 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,37 +6,15 @@ end
 
 
 def shipping_manifest
-   the_manifest  = {"whale bone corsets","whale bone corsets", "whale bone corsets", "whale bone corsets","whale bone corsets", "porcelain vases","porcelain vases", "oil paintings", "oil paintings", "oil paintings"}
-  
-
-
+   the_manifest  = {"clothing" => "whale bone corsets","clothing" => "whale bone corsets", "clothing" => "whale bone corsets", "clothing" => "whale bone corsets", "clothing" => "whale bone corsets", "pottery" => "porcelain vases", "pottery" => "porcelain vases", "artwork" => "oil paintings", "artwork" => "oil paintings", "artwork" => "oil paintings"}
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
+  shipping_manifest ["artwork"]
+ end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2  
+  shipping_manifest["gun powder"] = 4
+  puts shipping_manifest
 end
